8ecc8f224be51e1e0d84e5581815fb1f1b36b91d
tu7hnsxs1 <tu7hnsxs1@code.edu.az>
2022-03-15 11:11:45 +0400

    add

---
+++ Task.weapon/Task.weapon/Model/Weapon.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task.weapon.Model
+{
+    class Weapon
+    {
+        public int bulletCapacity;
+        public int bulletsCount;
+        public double dischangeSeconds;
+        public bool shootingMode;
+
+        public Weapon(int bulletCapacity, int bulletsCount, double dischangeSeconds, bool shootingMode)
+        {
+            this.bulletCapacity = bulletCapacity;
+            this.bulletsCount = bulletsCount;
+            this.dischangeSeconds = dischangeSeconds;
+            this.shootingMode = shootingMode;
+        }
+        public void Shoot()
+        {
+
+            if (bulletsCount > 0)
+
+            {
+                bulletsCount = bulletsCount - 1;
+                Console.WriteLine("Diw diw");
+            }
+            else if (bulletsCount == 0)
+            {
+                Console.WriteLine("Gulle bitib");
+            }
+            else
+            {
+                Console.WriteLine("Sayi menfi ola bilmez");
+            }
+
+        }
+        public void Fire()
+
+        {
+            if (dischangeSeconds > 0 && bulletsCount > 0 && bulletCapacity > 0)
+            {
+                double Second= (bulletsCount * dischangeSeconds) / bulletCapacity;
+                Console.WriteLine($"{Second} - Saniyeye bitir");
+            }
+            else
+            {
+                Console.WriteLine("Yalnis daxil edilib!");
+            }
+
+
+        }
+        public void GetRemainBulletCount()
+        {
+            if (bulletCapacity > 0 && bulletsCount > 0)
+            {
+                Console.WriteLine($"{bulletCapacity - bulletsCount} - Daragin dolmasi ucun lazim olan gulle sayi");
+            }
+            else
+            {
+                Console.WriteLine("Yalnis daxil edilib!");
+            }
+
+        }
+        public void Reload()
+        {
+            if (bulletCapacity > 0 && bulletsCount > 0)
+            {
+                Console.WriteLine($"{(bulletCapacity - bulletsCount) + bulletsCount} - Daraq yeniden doldu");
+            }
+            else
+            {
+                Console.WriteLine("Yalnis daxil edilib!");
+            }
+
+        }
+        public void changefiremode()
+        {
+            if (shootingMode == false)
+            {
+                Console.WriteLine("Avtomatik");
+                shootingMode = true;
+            }
+            else
+            {
+                Console.WriteLine("Tekdir");
+                shootingMode = false;
+            }
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Daragin gulle tutumu:{bulletCapacity }");
+            Console.WriteLine($"Daraqdaki gulle sayi:{bulletsCount }");
+            Console.WriteLine($"Daragin bosalma saniyesi:{dischangeSeconds }");
+            Console.WriteLine($"Atis modu:{shootingMode }");
+
+
+        }
+    }
+}
---
+++ Task.weapon/Task.weapon/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Task.weapon.Model
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon weapon = new Weapon(30, 10, 0.2, true);
+            
+            int key = 0;
+            do
+            {
+                Console.WriteLine("Reqem daxil edin : ");
+                key = Convert.ToInt32(Console.ReadLine());
+                switch (key)
+                {
+                    case 0:
+                        weapon.ShowInfo();
+                        break;
+                    case 1:
+                        weapon.Shoot();
+                        break;
+                    case 2:
+                        weapon.Fire();
+                        break;
+                    case 3:
+                        weapon.GetRemainBulletCount();
+                        break;
+                    case 4:
+                        weapon.Reload();
+                        break;
+                    case 5:
+                        weapon.changefiremode();
+                        break;
+                    case 6:
+                        Console.WriteLine("Sagolun");
+                        break;
+                    default:
+                        Console.WriteLine("Yalnis daxil etdiniz");
+                        break;
+                }
+            } while (key != 6);
+        }
+    }
+}
